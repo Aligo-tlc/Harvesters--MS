255a0e4373c0a9c0482b6c77a39a0db1a2f034a2
Aligo-tlc <guyaemmanuel@gmail.com>
2022-03-11 20:32:14 +0300

    first commit

---
+++ library/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> Harvesters Library Management System |<a href="https://www.facebook.com/emmanuelaligo.tlc/" target="_blank"  > Designed by : Emmanuel Aligo</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
